89487e784ded8734b52b3eb26c011ac73bc9ba3f
Wim Van Leuven <wim.vanleuven@bigboards.io>
2016-11-12 13:45:46 +0100

    wait for HDFS to startup

--- docker_files/oozie-server-run.sh
+++ docker_files/oozie-server-run.sh
@@ -7,6 +7,7 @@ install -d -o oozie -g oozie /var/tmp/oozie
 /usr/lib/oozie/bin/ooziedb.sh create -run
 
 echo "setup oozie sharelib to ${OOZIE_SHARELIB_URI}"
+hdfs --config "${OOZIE_HADOOP_CONF_DIR}" dfsadmin -safemode wait
 /usr/lib/oozie/bin/oozie-setup.sh sharelib create -fs "${OOZIE_SHARELIB_URI}" -locallib /usr/lib/oozie/oozie-sharelib-yarn
 
 /usr/lib/oozie/bin/oozied.sh run
