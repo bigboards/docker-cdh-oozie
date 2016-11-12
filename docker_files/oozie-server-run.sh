d941c345894f1a76e586033cf0c1c12ded1158a3
Wim Van Leuven <wim.vanleuven@bigboards.io>
2016-11-12 10:35:41 +0100

    setup share lib via env var

--- docker_files/oozie-server-run.sh
+++ docker_files/oozie-server-run.sh
@@ -5,5 +5,5 @@ install -d -o oozie -g oozie /var/run/oozie
 install -d -o oozie -g oozie /var/log/oozie
 install -d -o oozie -g oozie /var/tmp/oozie
 /usr/lib/oozie/bin/ooziedb.sh create -run
-/usr/lib/oozie/bin/oozie-setup.sh sharelib create -fs ${OZIE_SHARELIB_URI}
+/usr/lib/oozie/bin/oozie-setup.sh sharelib create -fs "${OZIE_SHARELIB_URI}"
 /usr/lib/oozie/bin/oozied.sh run
